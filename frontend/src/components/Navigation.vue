363b0bfa6eccf9cd5d6fc84af44c6fa9051959f9
codeninjah <codeninjahblog@gmail.com>
2021-02-23 22:08:35 +0100

    Nav test

--- frontend/src/components/Navigation.vue
+++ frontend/src/components/Navigation.vue
@@ -2,6 +2,7 @@
     <div class="wrapper">
         <div class="navbar">
             <img src="@/assets/sinus-logo.svg">
+            <img src="@/assets/icon-user-black.svg">
         </div>
     </div>
 </template>
@@ -15,5 +16,10 @@ export default {
 
 
 <style scoped>
+
+img {
+    height: 100px;
+    width: auto;
+}
 
 </style>
