ed1401ee3ce10bc776bb9b34edc2b1e6287addcf
AbbasMansoori <abbas.mansoori@iths.se>
2021-02-25 13:05:23 +0000

    test

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="about">
-<<<<<<< HEAD
-    <h1>This is an about page !!!</h1>
-=======
-    <h1>This is an about page!!!</h1>
->>>>>>> efb916d9ab00546207a8369442b8e8638c88915c
+    <h1>This is an about page!!</h1>
   </div>
 </template>
