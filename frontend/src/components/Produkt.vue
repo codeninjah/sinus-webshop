f670be13a54eb25822d053a5ab806dbe857838f7
codeninjah <codeninjahblog@gmail.com>
2021-02-26 14:49:25 +0100

    Progress, kind of, no errors atm

--- frontend/src/components/Produkt.vue
+++ frontend/src/components/Produkt.vue
@@ -7,6 +7,26 @@
         -->
 
         <p v-on:click=test>a</p>
+
+        <ul v-if="posts && posts.length">
+        <li
+            v-for="post of posts"
+            :key="post[0]"
+            >
+                {{ post.title }}
+                {{ post.price }}
+
+                {{post._id}}
+        </li>
+      </ul>
+
+
+        <ol v-if="errors && errors.length">
+            <li v-for="error of errors"
+            :key = "error">
+                <p>{{error.message}}</p>
+            </li>
+        </ol>
         
 
     </div>
@@ -14,11 +34,20 @@
 
 
 <script>
+import axios from 'axios';
+
 export default {
     name: "Produkt",
 
     //Här verkar ej product vara rätt prop
     props: ['title'],
+
+    data() {
+        return {
+            posts: [],
+            errors: [],
+        }
+    },
 
     methods : {
         test(){
@@ -27,6 +56,18 @@ export default {
     },
 
     created(){
+
+        //axios.get('http://localhost:5000/database/productsSeed.json')
+            axios.get("http://localhost:5000/api" + this.$route.path)
+            .then(response => {
+                this.posts = response.data
+            })
+            .catch(e => {
+                this.errors.push(e)
+            })
+
+
+        //console.log tests
         console.log(this.$route.params)
         console.log(this.$route.query)
         console.log(this.$router)
--- frontend/src/views/ProduktInfo.vue
+++ frontend/src/views/ProduktInfo.vue
@@ -10,12 +10,15 @@ import Produkt from "@/components/Produkt.vue";
 export default {
   components: { Produkt },
 
+  
+
   computed: {
     product() {
       let id = this.$route.params.id;
       let product = this.$store.getters.getProduct(id);
       return product;
     },
+
   },
 
 };
