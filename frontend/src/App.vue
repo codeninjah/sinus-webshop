8405b291f46e6b59deb3176b3d209b0c454f646f
AbbasMansoori <abbas.mansoori@iths.se>
2021-02-26 12:33:19 +0100

    ändringar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,22 @@
 <template>
   <div id="app">
-   
     <div class="overNav">
-      <img src="@/assets/sinus-logo.png" alt="">
+      <img src="@/assets/sinus-logo.png" alt="" />
 
-      <div class="totheright">
-        <img src="@/assets/cart.png" alt="">
-        <img src="@/assets/wiggly-sinus.png" alt="">
-        </div>
+      <section class="totheright">
+        <img src="@/assets/magnify.png" alt="" />
+        <router-link to="/myaccount" class="account-icon"><img class="account-icon" src="@/assets/user.png" alt="" /></router-link>
+        <router-link to="/kundkorg" class="cart-icon"><img class="cart-icon" src="@/assets/cart-white.png" alt=""/></router-link>
+        <img src="@/assets/wiggly-sinus.png" alt="" />
+      </section>
     </div>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/checkout">Checkout</router-link> |
-      <router-link to="/kundkorg">Kundkorg</router-link> |
+     
       <router-link to="/loggin">LoggIn</router-link> |
-      <router-link to="/myaccount">My Account</router-link> |
+       |
       <router-link to="/produkt">Produkt</router-link> |
       <router-link to="/produktlista">Produktlista</router-link> |
     </div>
@@ -26,11 +27,8 @@
   </div>
 </template>
 
-
 <script>
-export default {
-   
-}
+export default {};
 </script>
 
 <style lang="scss">
@@ -45,14 +43,15 @@ export default {
 #nav {
   padding: 30px;
   background-image: url(../src/assets/header-background.jpg);
-  
 
   a {
     font-weight: bold;
     color: orange;
-
+    
+    
     &.router-link-exact-active {
       color: #42b983;
+     
     }
   }
 }
@@ -68,25 +67,35 @@ export default {
 }
 
 .overNav {
-  
   display: flex;
   flex-direction: row;
-  
+
   justify-content: space-between;
   background-color: rgb(31, 29, 29);
   color: white;
   height: 2.5rem;
-
 }
 
-.totheright{
+.totheright {
   display: flex;
+  margin: 5px;
   flex-direction: row;
-  justify-content: flex-end;
+  justify-content: space-evenly;
+  width: 200px;
   
-
 }
 
+.cart-icon {
+  height: -webkit-fill-available;
+  display: flex;
+  justify-content: space-evenly;
+}
+
+.account-icon {
+  height: -webkit-fill-available;
+  display: flex;
+  justify-content: space-evenly;
+}
 html,
 body {
   background-color: #b72929;
