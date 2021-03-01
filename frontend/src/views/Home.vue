73ee531fdbe5b68bb3dccb8b4f415a9eafce774a
AbbasMansoori <abbas.mansoori@iths.se>
2021-03-01 20:38:30 +0100

    home page ska visa bilder men visar bara adressen än så länge

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,42 +1,73 @@
 <template>
   <div class="home">
     <div class="wrapper">
-    <a id="triangle" href="#"></a>
+      <a id="triangle" href="#"></a>
+    </div>
 
-  </div>
+    <ul class="hoodieTab" v-if="posts && posts.length">
+      <li
+        v-for="post in posts"
+        :key="post.imgFile"
+        @click="$router.push(`products/${post.imgFile}`)"
+      >
+      <img :src="post.imgFile">
+        
+
+       {{ post.imgFile}}
+
+       {{ post._id}}
+
+        <!--Printas ej -->
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
-
-
+import axios from "axios";
 export default {
-  name: 'Home',
-  components: {
-     
-  }
-}
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      posts: [],
+      errors: [],
+    };
+  },
+
+  created() {
+    //axios.get('http://localhost:5000/database/productsSeed.json')
+    axios
+      .get("http://localhost:5000/api/products/")
+      .then((response) => {
+        this.posts = response.data;
+      })
+      .catch((e) => {
+        this.errors.push(e);
+      });
+  },
+
+  computed: {
+    products() {
+      return this.$store.state.products;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
-
 .wrapper {
   display: flex;
   flex-direction: row;
-  justify-content:center;
+  justify-content: center;
 }
 
-
-#triangle{
-  
-	width: 0;
-	height: 0;
-	border-left: 25px solid transparent;
-	border-right: 25px solid transparent;
-	border-top: 50px solid black;
+#triangle {
+  width: 0;
+  height: 0;
+  border-left: 25px solid transparent;
+  border-right: 25px solid transparent;
+  border-top: 50px solid black;
 }
-
 </style>
--- frontend/src/views/ProduktLista.vue
+++ frontend/src/views/ProduktLista.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="wrapper">
-        <h1>Produkt Lista</h1>
+  <div class="wrapper">
+    <h1>Produkt Lista</h1>
 
-        <!--
+    <!--
         <ul v-if="posts && posts.length">
             <li v-for="(post, index) of posts"
             :key = "post"
@@ -12,49 +12,44 @@
         </ul>
         -->
 
-        <ul v-if="posts && posts.length">
-        <li
-            v-for="post in posts"
-            :key="post.id"
-            @click="$router.push(`products/${post._id}`)"
-            >
-                {{ post.title }}
-                {{ post.price }}
+    <ul v-if="posts && posts.length">
+      <li
+        v-for="post in posts"
+        :key="post.id"
+        @click="$router.push(`products/${post._id}`)"
+      >
+        {{ post.title }}
+        {{ post.price }}
 
-                {{post._id}}
-                {{post.id}} <!--Printas ej -->
+        {{ post._id }}
+        {{ post.id }}
+        <!--Printas ej -->
+      </li>
+    </ul>
 
-        </li>
-      </ul>
-
-
-        <ol v-if="errors && errors.length">
-            <li v-for="error of errors"
-            :key = "error">
-                <p>{{error.message}}</p>
-            </li>
-        </ol>
-
-    </div>    
+    <ol v-if="errors && errors.length">
+      <li v-for="error of errors" :key="error">
+        <p>{{ error.message }}</p>
+      </li>
+    </ol>
+  </div>
 </template>
 
-
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
+  //här kan det vara så att man inte skickat rätt data som ska tas emot av Produkt-componenten via props
+  data() {
+    return {
+      posts: [],
+      errors: [],
+    };
+  },
 
-    //här kan det vara så att man inte skickat rätt data som ska tas emot av Produkt-componenten via props
-    data() {
-        return {
-            posts: [],
-            errors: [],
-        }
-    },
+  name: "ProduktLista",
 
-    name: "ProduktLista",
-
-    /*
+  /*
     methods: {
         popUp(index){
             this.index = index
@@ -63,32 +58,29 @@ export default {
         }
     },
     */
-    
-    created() {
-            //axios.get('http://localhost:5000/database/productsSeed.json')
-            axios.get("http://localhost:5000/api/products/")
-            .then(response => {
-                this.posts = response.data
-            })
-            .catch(e => {
-                this.errors.push(e)
-            })
-         },
 
-    computed: {
-        products() {
-            return this.$store.state.products;
-        },
+  created() {
+    //axios.get('http://localhost:5000/database/productsSeed.json')
+    axios
+      .get("http://localhost:5000/api/products/")
+      .then((response) => {
+        this.posts = response.data;
+      })
+      .catch((e) => {
+        this.errors.push(e);
+      });
+  },
+
+  computed: {
+    products() {
+      return this.$store.state.products;
     },
-}
-
+  },
+};
 </script>
 
-
 <style scoped>
-
 ol {
-    list-style-type: georgian;
+  list-style-type: georgian;
 }
-
 </style>
