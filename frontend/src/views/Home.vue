aa037824c4631b3413772f2d3bda105fff45e949
codeninjah <codeninjahblog@gmail.com>
2021-03-02 14:17:28 +0100

    Work with modal

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="home">
+
     <div class="wrapper">
       <a id="triangle" href="#"></a>
     </div>
+
+    <div id="myModal" class="lightbox nodisplay">
+        <span class="close cursor" v-on:click="closeLightBox()">ALEX</span>
+          <div class="lightbox-content">
+
+            <img src="https://live.staticflickr.com/8515/8458051641_8563c83111.jpg" />
+            <img src="https://keepongeekin.files.wordpress.com/2020/08/cropped-1500x500-1.jpg" />
+
+      </div>
+    </div>
+
       <section class="listWrapper">
+
     <ul class="imglist" v-if="posts && posts.length">
+
       <router-link class="listItems" 
         v-for="post in posts"
         :key="post._id"
@@ -14,11 +28,14 @@
 
         {{post.title}}
         
-
+        
         <!--Printas ej -->
       </router-link>
+
     </ul>
     </section>
+
+    
   </div>
 </template>
 
@@ -46,6 +63,28 @@ export default {
         this.errors.push(e);
       });
   },
+
+  methods: {
+      openLightBox() {
+            const open = document.getElementById("myModal")
+            open.style.display = "block"
+
+            //console.log(images)
+
+        //console.log(this.$root.imgArray)
+        //const array = this.$root.imgArray
+
+        },
+
+        closeLightBox(){  
+            const open = document.getElementById("myModal")
+            open.style.display = "none"
+            
+        //alert("Bra!")
+
+        },
+
+    },
 
   computed: {
     
@@ -81,8 +120,7 @@ export default {
 }
 
 .listItems {
-  
-      display: flex;
+    display: flex;
     flex-direction: column;
     width: 200px;
     height: 200px;
@@ -101,6 +139,70 @@ export default {
 .listWrapper {
   min-height: 800px;
   margin:2%
+}
+
+/* CODE FOR MODAL */
+#myModal {
+    border: 3px solid black;
+    width: 100%;
+    height: auto;
+}
+
+/* The Lightbox (background) */
+.lightbox {
+    display: none;
+    position: fixed;
+    z-index: 1;
+    margin-top: 100px;
+    margin-left: 100px;
+    margin-right: 100px;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: black;
+}
+
+/* Lightbox Content */
+.modal-content {
+  position: relative;
+  background-color: #fefefe;
+  margin: auto;
+  padding: 0;
+  width: 90%;
+  max-width: 1200px;
+}
+
+.display {
+    display: block;
+}
+
+.nodisplay {
+    display: none;
+}
+
+
+.main-wrapper {
+    border: 3px solid black;
+    width: 300px;
+    height: 300px;
+}
+
+/* The Close Button */
+.close {
+  color: white;
+  /*position: absolute;*/
+  top: 10px;
+  right: 25px;
+  font-size: 35px;
+  font-weight: bold;
+}
+
+
+
+/* Hide the slides by default */
+.mySlides {
+  display: none;
 }
 
 </style>
