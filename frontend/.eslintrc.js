308763ae135d957202f6bb0ebfa58746a3ddc203
codeninjah <codeninjahblog@gmail.com>
2021-03-04 14:32:47 +0100

    README added

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,3 +1,4 @@
+/*
 module.exports = {
   root: true,
   env: {
@@ -15,3 +16,19 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 }
+*/
+
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  }
+};
