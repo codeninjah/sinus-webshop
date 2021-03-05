9cdcc18ff6c39b1c7f5ff76dd816d968ee30e101
codeninjah <codeninjahblog@gmail.com>
2021-03-05 20:35:43 +0100

    Cartview progress

--- frontend/src/views/KundKorg.vue
+++ frontend/src/views/KundKorg.vue
@@ -1,13 +1,20 @@
 <template>
     <div class="wrapper">
         <h1>Kund korg</h1>
+        <div>{{getCart}}</div>
     </div>
 </template>
 
 
 <script>
 export default {
-    name: "KundKorg"
+    name: "KundKorg",
+
+    computed:{
+        getCart(){
+            return this.$store.getters.getCart
+        }
+    }
 }
 </script>
 
