b6d05adf83c42f50dc1acd23a094e4159da05f37
codeninjah <codeninjahblog@gmail.com>
2021-03-05 20:39:35 +0100

    Cart view

--- frontend/src/views/KundKorg.vue
+++ frontend/src/views/KundKorg.vue
@@ -2,6 +2,12 @@
     <div class="wrapper">
         <h1>Kund korg</h1>
         <div>{{getCart}}</div>
+        <h2>Här loopar vi</h2>
+        <h3>Och skriver ut namnen</h3>
+            <div v-for="product in getCart"
+                :key="product.id">
+                {{product.title}}
+            </div>
     </div>
 </template>
 
