bd9671c559c27a932ad5cd6a6815919bdf686588
AbbasMansoori <abbas.mansoori@iths.se>
2021-03-06 12:45:09 +0100

    ss

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -18,22 +18,3 @@ module.exports = {
 }
 
 
-<<<<<<< HEAD
-=======
-/*
-module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint"
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
-};
-*/
->>>>>>> 2666da6025f98b8051c1d925abe1c486249153b3
