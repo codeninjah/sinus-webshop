54bd912026bfdff1ba6418b74e97ca1d73a9d7dd
AbbasMansoori <abbas.mansoori@iths.se>
2021-03-09 12:05:12 +0100

    up to date

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -38,7 +38,6 @@ export default new Vuex.Store({
     ADD_TO_CART(state, payload) {
       state.cart.push(payload)
       console.log(state.cart)
-    },
     //User stuff
     CURRENT_USER(state, payload) {
       state.user = payload
