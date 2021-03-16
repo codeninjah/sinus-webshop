a67d7b5c60c9539d4fa115a863674342357ae15f
AbbasMansoori <abbas.mansoori@iths.se>
2021-03-16 19:53:49 +0100

    k

--- frontend/src/views/KundKorg.vue
+++ frontend/src/views/KundKorg.vue
@@ -1,23 +1,5 @@
 <template>
-<<<<<<< HEAD
-
-
-
-    
-    <div class="wrapper">
-        <h1>Kund korg</h1>
-        <div>{{getCart}}</div>
-        <h2>Här loopar vi</h2>
-        <h3>Och skriver ut namnen</h3>
-            <div v-for="product in getCart"
-                :key="product.id">
-                {{product.title}}
-                {{product.price}}
-            </div>
-    </div>
-=======
    <div></div>
->>>>>>> 53bccafe117807189e168c33ccc2331ec5f43b23
 </template>
 
 
