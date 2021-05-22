52386078c01f0b18b2053d4fcd44bc3c0721d7ab
codeninjah <codeninjahblog@gmail.com>
2021-05-22 19:28:00 +0200

    More progress

--- frontend/src/views/Checkout.vue
+++ frontend/src/views/Checkout.vue
@@ -1,14 +1,28 @@
 <template>
     <div class="wrapper">
         <h1>Checkout vyn</h1>
+        <ul>
+            <li v-for="product in getCart"
+                :key="product.id">
+                {{product.title}}
+                {{product.price}}
+            </li>
+        </ul>
     </div>
 </template>
 
 
 <script>
 export default {
-    name: "Checkout"
+    name: "Checkout",
+
+    computed:{
+        getCart(){
+            return this.$store.getters.getCart
+        }
+    },
 }
+
 </script>
 
 
